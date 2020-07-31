406743216409577d3efce63d812ebe8d68fc25e1
sherry999 <sherry999@cau.ac.kr>
2020-07-31 12:43:58 +0900

    quiz목록이랑 랭킹

--- quizes/views.py
+++ quizes/views.py
@@ -1,3 +1,9 @@
+import data as data
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'quizes/index.html', data)
+
+def detail(request):
+    return render(request, 'quizes/detail.html', data)
