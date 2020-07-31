9a4a17660b707da78f00149ebf7924f3140d156b
awesomeo184 <awesomeo184@gmail.com>
2020-07-31 18:34:12 +0900

    merge complete

--- config/settings.py
+++ config/settings.py
@@ -94,15 +94,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-<<<<<<< HEAD
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-=======
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
->>>>>>> origin/yurim
+
 ]
 
 # Internationalization
--- quizes/urls.py
+++ quizes/urls.py
@@ -6,7 +6,7 @@ app_name = "quizes"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("quiz/create/", views.make_quiz, name="make-quiz"),
+    path("create/", views.make_quiz, name="make-quiz"),
     path("users/<int:pk>", views.solve_quiz, name="solve-quiz"),
     path('list/', views.QuizList.as_view()),
     path('ranking/', views.RankingList.as_view()),
