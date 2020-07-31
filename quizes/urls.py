a64092afee3cf245da73a384b4222e236ad7343b
yejin-park <tsi06162@naver.com>
2020-07-31 18:05:55 +0900

    1차 모으기

--- quizes/urls.py
+++ quizes/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
+from . import views
 
 app_name = "quizes"
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('list/', views.list, name='list'),
+    path('create/', views.create, name='create'),
+    path('quiz/<int:question_id>/', views.quiz, name='quiz'),
+    path('detail<int:question_id>/', views.detail, name='detail'),
+]
 
--- quizes/views.py
+++ quizes/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Question
+
+def home(request):
+    return HttpResponse("making views")
+
+def list(request):  ################
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = ' || '.join([q.question_text for q in latest_question_list])
+   # context = {'latest_question_list': latest_question_list}
+    return HttpResponse(context)
+   # return render(request, 'quizes/list.html', context)
+
+
+
+def create(request):
+    return HttpResponse("create")
+
+def quiz(request, question_id):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'quizes/quiz.html', context)
+
+
+## 위아래 question_id 대응
+def detail(request, question_id):
+    return HttpResponse("detail")
